1b807ea4fd61998b7b94d28d74c29b83c27a7cf2
ignacioarroyo <ignacioarroyo17@gmail.com>
2023-01-31 23:15:48 -0300

    input search navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -13,6 +13,9 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Link from '@mui/material/Link';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from '../../auth/context/AuthContext';
 
@@ -74,6 +77,46 @@ const Navbar = () => {
       });
       }
 
+  const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
       return (
         <>
         <AppBar position="static">
@@ -97,6 +140,7 @@ const Navbar = () => {
               >
                 Tinky
               </Typography>
+              
     
               <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                 <IconButton
@@ -157,7 +201,7 @@ const Navbar = () => {
                   textDecoration: 'none',
                 }}
               >
-                LOGO
+                Tinky
               </Typography>
               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                 {/* {pages.map((page) => (
@@ -184,6 +228,15 @@ const Navbar = () => {
                     Perfil
                   </Button> */}
               </Box>
+              <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Buscar..."
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
               {userAuth?(<>
                 <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Open settings">
