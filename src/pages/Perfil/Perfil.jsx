826a9558e1732f480960567ba1098d491c178596
jpsoria6 <jpsoria6@hotmail.com>
2023-02-01 00:15:12 -0300

    enhancement Perfil page and create of Opinion component

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -9,7 +9,15 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import Rating from '@mui/material/Rating';
 import Toolbar from '@mui/material/Toolbar';
+import ErrorIcon from '@mui/icons-material/Error';
+import HelpIcon from '@mui/icons-material/Help';
+import Opinion from '../../components/Opinion/Opinion';
 
+const opiniones = [
+  {nombre:'Ignacio Arroyo', puntaje: 4, comentario: 'Excelente Profesional'},
+  {nombre:'Juan Pablo Soria', puntaje: 5, comentario: 'Sin comentarios muy buen servicio'},
+  {nombre:'Omar Rivas', puntaje: 2, comentario: 'Llega tarde a las reuniones'},
+]
 
 
 const Perfil = () => {
@@ -38,41 +46,13 @@ const Perfil = () => {
 <Typography variant="subtitle1" color="text.secondary">
               215 horas / 304 sesiones
             </Typography>
+  <Rating name="rating-read-only" value={4} readOnly />
 </Toolbar>
 
             </Stack>    
 
       {/* <CardActionArea>
         <Card sx={{ display: 'flex' }}> */}
-        <Stack direction="row" justifyContent="between">
-
-        
-          <CardContent sx={{flex:1, my:2, }}>
-          <Typography align='center' variant="h6" color="text.primary" sx={{ my:1 }}>
-          20 u$s por hora
-            </Typography>
-            <Typography align='center' variant="subtitle1" color="text.primary" sx={{ my:1 }}>
-              Argentina
-            </Typography>
-            <Typography align='center' variant="subtitle1" color="text.primary" sx={{ my:1 }}>
-              Capital Federal
-            </Typography>
-          </CardContent>
-          <CardContent sx={{flex:1, my:2, }}>
-            <Typography align='center' variant="subtitle1" color="text.secondary" sx={{ my:1 }}>
-            <Rating name="rating-read-only" value={4} readOnly />
-            </Typography>
-            <Typography align='center' variant="subtitle1" color="text.secondary" sx={{ my:1 }}>
-              8.0 / 10
-            </Typography>
-            <Typography align='center' variant="subtitle1" color="text.secondary" sx={{ my:1 }}>
-              (2100 opiniones)
-            </Typography>
-            <Typography align='center' variant="subtitle1" color="text.secondary" sx={{ my:1 }}>
-              ESP / ING
-            </Typography>
-          </CardContent>
-          </Stack>
           <Stack sx={{ my:1, mx:5 }}>
           <Typography variant="h6" color="text.secondary" sx={{ my:1}}>
           Descripcion
@@ -81,6 +61,38 @@ const Perfil = () => {
               Busco el bienestar completo de mis pacientes. Atiendo a hombres y mujeres.
             </Typography>
           </Stack>
+          <Stack direction="row" justifyContent="between" sx={{background:'#ffd066'}}>
+            <CardContent sx={{flex:1, my:2, }}>
+              <Typography sx={{display:'flex',justifyContent: 'center', alignItems: 'center'}}>
+              <ErrorIcon/>
+                CV
+              </Typography>
+            </CardContent>
+            <CardContent sx={{flex:1, my:2, }}>
+            <Typography sx={{display:'flex',justifyContent: 'center', alignItems: 'center'}}>
+              <HelpIcon/>
+                Opiniones
+              </Typography>
+            </CardContent>
+          </Stack>
+
+          <Stack direction="row" justifyContent="between" >
+            <CardContent sx={{flex:1, my:2, }}>
+              <Typography paragraph sx={{display:'flex',justifyContent: 'center', alignItems: 'center'}}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quasi eius, magni, ipsum eum aut accusamus sint earum autem delectus exercitationem odit officiis mollitia fuga tempore fugiat quae porro voluptatem 
+                repellat.
+              </Typography>
+            </CardContent>
+            <CardContent sx={{flex:1, my:2, }}>
+            <Typography sx={{display:'block',justifyContent: 'center', alignItems: 'center'}}>
+                {opiniones.map((op, i) => {
+                  return <Opinion nombre={op.nombre} puntaje={op.puntaje} comentario={op.comentario}/>
+                })}
+              </Typography>
+            </CardContent>
+          </Stack>
+        
         </Card>
       </CardActionArea>
       </Container>
